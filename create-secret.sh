9e41ce26cb21c7c6ac7fa2d36940012579193c98
Marlon Nunes <marlonnunes869@gmail.com>
2024-08-05 22:30:30 -0300

    fix: small adjustments

--- create-secret.sh
+++ create-secret.sh
@@ -5,6 +5,6 @@ export AWS_DEFAULT_REGION=us-east-1
 JSON=$(jq -n \
           --arg my_secret "$SECRET_KEY" \
           '{
-              "my_secret_key": my_secret
+              "my-secret-key": $my_secret
           }')
 aws --endpoint-url=http://localstack:4566 secretsmanager create-secret --name /secret/localstack --secret-string "$JSON"
