e48e0dc1da51b3cb26c25e170aa5cf55fe501313
Marlon Nunes <marlonnunes869@gmail.com>
2024-08-05 22:31:11 -0300

    feat: added secret manager configuration

--- src/main/java/com/marlonnunes/localstack/LocalstackApplication.java
+++ src/main/java/com/marlonnunes/localstack/LocalstackApplication.java
@@ -1,5 +1,7 @@
 package com.marlonnunes.localstack;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,13 @@ public class LocalstackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalstackApplication.class, args);
+	}
+
+	@Value("${my-secret-key}")
+	String mySecretKey;
+	@PostConstruct
+	public void init(){
+		System.out.println("My secret key is: " + mySecretKey);
 	}
 
 }
